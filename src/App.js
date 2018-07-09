2ef9dbb5d3e8522694cea496d068170aacec63fb
sgwanna <sg0314@castis.com>
2018-07-09 14:34:44 +0900

    commit first

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+
+class App extends Component {
+
+  // Render cycle: componentWillMount() -> render() -> componentDidMount();
+  // Update cycle: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  // 컴포넌트 안에 state가 변경될 때 마다 render 발생
+  // state 변경 시 직접 변경 X, setState로 변경해야 함
+
+  state = {}
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _getMovies = async () => {              // async 안쓰면 awaie 작동 안함
+    const movies = await this._callApi()  // callApi가 완료될 때까지 기다린 후 movies에 넣는다
+    this.setState({
+      movies    // movies : movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(tomato => tomato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie key={movie.id}
+                    title={movie.title}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                    synopsis = {movie.synopsis}
+            />
+    });
+
+    return movies;
+  }
+
+  render() {
+    const {movies} = this.state;
+    return (
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import './Movie.css';
+/*
+class Movie extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
+
+  render() {
+//    console.log(this.props);
+    return (
+      <div>
+        <MoviePoster poster={this.props.poster}/>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+*/
+function Movie({title, poster, genres, synopsis}) {
+  return (
+    <div className="Movie">
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title}/>
+      </div>
+      <div className="Movie_Columns">
+          <h1>{title}</h1>
+          <div className="Movie_Genres">
+            {genres.map((genre,index) => <MovieGenre genre={genre} key={index}/>)}
+          </div>
+          <div className="Movie_Synopsis">
+            <LinesEllipsis
+              text={synopsis}
+              maxLine='3'
+              ellipsis=' ...'
+              trimRight
+              basedOn='letters'
+            />
+          </div>
+      </div>
+    </div>
+  )
+}
+
+function MovieGenre({genre}){
+  return (
+    <span className="Movie_Genre">{genre} </span>
+  )
+}
+
+
+/*
+class MoviePoster extends Component {
+  static propTypes = {
+    poster: PropTypes.string.isRequired
+  }
+
+  render() {
+    return(
+      <img src={this.props.poster}/>
+    );
+  }
+}
+*/
+// stateless functional component
+function MoviePoster({poster, alt}) {
+  return(
+    <img src={poster} alt={alt} title={alt} className="Movie_Poster"/>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
+}
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
+}
+MoviePoster.propsTypes = {
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+export default Movie;
